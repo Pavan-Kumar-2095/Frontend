a4be4e9992a1d68952cc3093495b879fcf093834
Pavan-Kumar-2095 <developer0exe@gmail.com>
2024-12-28 17:27:02 +0530

    css changes

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -27,8 +27,8 @@ export default function Item() {
       <div className='page'>
       <h3 className='headlines'>{content[0].headlines}</h3>
       <h4 className='date'>{content[0].date}</h4>
-      <div className='img' >
-        <img  src={content[0].URL} alt="img" className="img" ></img>
+      <div className='img-container' >
+        <img  src={content[0].URL} alt="Image" ></img>
       </div>
       <h5 className='content'  style={{ color:"#e9e8e3" , backgroundColor:" rgba(2, 135, 202, 0.1)"}}>{content[0].content}</h5>
       <h3 className='headlines'>post by {content[0].AuthorName}</h3>
