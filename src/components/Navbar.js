40775da6654f86a3cd58250d52770a518611573f
Pavan-Kumar-2095 <developer0exe@gmail.com>
2026-02-26 22:45:05 +0530

    Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,37 @@
-import {React, useState} from 'react'
-import {Link} from 'react-router-dom'
-import './Navbar.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
 export default function MainPage() {
-
-  const  [ vis , setvis] = useState("visible");
+  const [isOpen, setIsOpen] = useState(false); // mobile menu toggle
   const storedValue = sessionStorage.getItem("username");
-  const cansee = async ()=>{
 
-    if(vis==="visible"){
-      setvis("hidden")
-    }
-    else{
-      setvis("visible")
-    }
-  }
-  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='nav'>
-      <div className='logo' >
-       <h1 style={{textDecoration:"none"}} className='logor' onClick={cansee}   >Global Insight</h1>
+      {/* Logo on the left */}
+      <div className='logo'>
+        <h1 className='logor'>Global Insight</h1>
       </div>
-      <div className='option' style={{ visibility:vis}}  >
-        <Link to='/' style={{ textDecoration: 'none' , color: '#edf2f4' ,  visibility:vis}} className='links' ><h4 style={{ visibility:vis}}>HOME</h4></Link>
-        <Link to='/news' style={{ textDecoration: 'none' , color: '#edf2f4' ,  visibility:vis }} className='links' ><h4 style={{ visibility:vis}} >NEWS</h4></Link>
-        <Link to='/add'style={{ textDecoration: 'none' , color: '#edf2f4',  visibility:vis}} className='links' ><h4 style={{ visibility:vis}}>ADD</h4></Link>
-        <Link to='/delete' style={{ textDecoration: 'none' , color: '#edf2f4',  visibility:vis }} className='links'><h4 style={{ visibility:vis}} >DELETE</h4></Link>
-        <Link to='/' style={{ textDecoration: 'none' , color: '#edf2f4' ,  visibility:vis}} className='links' ><h4 style={{ visibility:vis}}> {storedValue}</h4> </Link>
+
+      {/* Hamburger menu on right (mobile only) */}
+      <div className='hamburger' onClick={toggleMenu}>
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
+
+      {/* Links Section */}
+      <div className={`option ${isOpen ? 'open' : ''}`}>
+        <Link to='/' className='links'>HOME</Link>
+        <Link to='/news' className='links'>NEWS</Link>
+        <Link to='/add' className='links'>ADD</Link>
+        <Link to='/delete' className='links'>DELETE</Link>
+        <Link to='/' className='links'>{storedValue}</Link>
       </div>
     </div>
-  )
+);
 }
